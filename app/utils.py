7e598fde1c69574f929656e5a04c4e8d114e639e
lomolisso <raul.delafuente@ing.uchile.cl>
2024-08-13 10:32:25 -0400

    refactor esn v3.0

--- app/utils.py
+++ app/utils.py
@@ -1,62 +1,138 @@
 import redis
 from collections import deque
 import json
-from app.core.config import MAX_INFERENCE_QUEUE_SIZE, PREDICTION_HISTORY_LENGTH, ABNORMAL_LABELS, NORMAL_PREDICTION_THRESHOLD, ABNORMAL_PREDICTION_THRESHOLD, CLOUD_INFERENCE_LAYER, GATEWAY_INFERENCE_LAYER, SENSOR_INFERENCE_LAYER, HEURISTIC_ERROR_CODE, ADAPTIVE_INFERENCE, MAX_INFERENCE_QUEUE_SIZE
+from app.api.schemas import PredictionResult
+from redis import Redis
+from app.core.config import (
+    MAX_INFERENCE_QUEUE_SIZE,
+    PREDICTION_HISTORY_LENGTH,
+    ABNORMAL_LABELS,
+    NORMAL_PREDICTION_THRESHOLD,
+    ABNORMAL_PREDICTION_THRESHOLD,
+    CLOUD_INFERENCE_LAYER,
+    GATEWAY_INFERENCE_LAYER,
+    SENSOR_INFERENCE_LAYER,
+    HEURISTIC_ERROR_CODE,
+    ADAPTIVE_INFERENCE,
+    MAX_INFERENCE_QUEUE_SIZE,
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_DB_HISTORY,
+    REDIS_DB_CELERY_BROKER,
+)
+
+class RedisContextManager:
+    def __init__(self, *args, **kwargs):
+        self.redis = redis.Redis(*args, **kwargs)
+
+    def __enter__(self):
+        return self.redis
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.redis.close()
+
+def _get_prediction_queue_size() -> int:
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_CELERY_BROKER
+    ) as redis_client:
+        return redis_client.llen("prediction_queue")
+
 
 def _is_prediction_abnormal(prediction) -> int:
     """
     If the prediction is abnormal, return 1. Otherwise, return 0.
     """
     return prediction in ABNORMAL_LABELS
-    
-def _get_prediction_counter(redis_client: redis.Redis, gateway_name: str, sensor_name: str):
-    key = f"counter:{gateway_name}:{sensor_name}"
-    _redis_value = redis_client.get(key)
-    prediction_counter = int(_redis_value) if _redis_value else 0
 
-    return prediction_counter
 
-def _set_prediction_counter(redis_client: redis.Redis, gateway_name: str, sensor_name: str, prediction_counter: int):
-    key = f"counter:{gateway_name}:{sensor_name}"
-    redis_client.set(key, prediction_counter)
+def _get_prediction_counter(gateway_name: str, sensor_name: str):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"counter:{gateway_name}:{sensor_name}"
+        _redis_value = redis_client.get(key)
+        prediction_counter = int(_redis_value) if _redis_value else 0
 
-def update_prediction_counter(redis_client: redis.Redis, gateway_name: str, sensor_name: str):
-    prediction_counter = _get_prediction_counter(redis_client, gateway_name, sensor_name)
+        return prediction_counter
+
+
+def _set_prediction_counter(
+    gateway_name: str,
+    sensor_name: str,
+    prediction_counter: int,
+):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"counter:{gateway_name}:{sensor_name}"
+        redis_client.set(key, prediction_counter)
+
+
+def update_prediction_counter(gateway_name: str, sensor_name: str):
+    prediction_counter = _get_prediction_counter(
+        gateway_name, sensor_name
+    )
     prediction_counter += 1
-    _set_prediction_counter(redis_client, gateway_name, sensor_name, prediction_counter)
+    _set_prediction_counter(gateway_name, sensor_name, prediction_counter)
 
     return prediction_counter
 
-def _get_prediction_history(redis_client: redis.Redis, gateway_name: str, sensor_name: str):
-    key = f"history:{gateway_name}:{sensor_name}"
-    _redis_value = redis_client.get(key)
-    prediction_history = deque(json.loads(_redis_value), maxlen=PREDICTION_HISTORY_LENGTH) if _redis_value else deque(maxlen=PREDICTION_HISTORY_LENGTH)
 
-    return prediction_history
+def _get_prediction_history(gateway_name: str, sensor_name: str):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"history:{gateway_name}:{sensor_name}"
+        _redis_value = redis_client.get(key)
+        prediction_history = (
+            deque(json.loads(_redis_value), maxlen=PREDICTION_HISTORY_LENGTH)
+            if _redis_value
+            else deque(maxlen=PREDICTION_HISTORY_LENGTH)
+        )
 
-def _set_prediction_history(redis_client: redis.Redis, gateway_name: str, sensor_name: str, prediction_history: deque):
-    key = f"history:{gateway_name}:{sensor_name}"
-    redis_client.set(key, json.dumps(list(prediction_history)))
+        return prediction_history
 
 
-def update_prediction_history(redis_client: redis.Redis, gateway_name: str, sensor_name: str, prediction: int):
+def _set_prediction_history(
+    gateway_name: str,
+    sensor_name: str,
+    prediction_history: deque,
+):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"history:{gateway_name}:{sensor_name}"
+        redis_client.set(key, json.dumps(list(prediction_history)))
+
+
+def update_prediction_history(gateway_name: str, sensor_name: str, prediction: int):
     is_abnormal: bool = _is_prediction_abnormal(prediction)
-    prediction_history = _get_prediction_history(redis_client, gateway_name, sensor_name)
+    prediction_history = _get_prediction_history(
+        gateway_name, sensor_name
+    )
     prediction_history.append(1 if is_abnormal else 0)
-    _set_prediction_history(redis_client, gateway_name, sensor_name, prediction_history)
+    _set_prediction_history(gateway_name, sensor_name, prediction_history)
 
     return prediction_history
 
-def clear_prediction_counter(redis_client: redis.Redis, gateway_name: str, sensor_name: str):
-    key = f"counter:{gateway_name}:{sensor_name}"
-    redis_client.delete(key)
 
-def clear_prediction_history(redis_client: redis.Redis, gateway_name: str, sensor_name: str):
-    key = f"history:{gateway_name}:{sensor_name}"
-    redis_client.delete(key)
+def clear_prediction_counter(gateway_name: str, sensor_name: str):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"counter:{gateway_name}:{sensor_name}"
+        redis_client.delete(key)
 
 
-def gateway_adaptive_inference_heuristic(redis_client: redis.Redis, gateway_name: str, sensor_name: str, inf_queue_size: int, low_battery: bool, prediction: int) -> int:
+def clear_prediction_history(gateway_name: str, sensor_name: str):
+    with RedisContextManager(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_HISTORY
+    ) as redis_client:
+        key = f"history:{gateway_name}:{sensor_name}"
+        redis_client.delete(key)
+
+
+def gateway_adaptive_inference_heuristic(prediction_result: PredictionResult) -> int:
     """
     Gateway Adaptive Inference Heuristic
 
@@ -68,16 +144,22 @@ def gateway_adaptive_inference_heuristic(redis_client: redis.Redis, gateway_name
     phi_g: threshold for normal predictions, if less than phi_g, set inference layer to sensor
     psi_g: threshold for abnormal predictions, if greater than psi_g, set inference layer to cloud
     """
-    u_t = update_prediction_counter(redis_client, gateway_name, sensor_name)
-    prediction_history = update_prediction_history(redis_client, gateway_name, sensor_name, prediction)
+
+    gateway_name = prediction_result.gateway_name
+    sensor_name = prediction_result.sensor_name
+    low_battery = prediction_result.low_battery
+    prediction = prediction_result.prediction
+
+    u_t = update_prediction_counter(gateway_name, sensor_name)
+    prediction_history = update_prediction_history(
+        gateway_name, sensor_name, prediction
+    )
     assert u_t >= len(prediction_history)
-    
-    m = PREDICTION_HISTORY_LENGTH 
     sigma_M_t = sum(prediction_history)
+    q_t = _get_prediction_queue_size()
 
-    q_t = inf_queue_size
+    m = PREDICTION_HISTORY_LENGTH
     psi_q = MAX_INFERENCE_QUEUE_SIZE
-
     phi_g = NORMAL_PREDICTION_THRESHOLD
     psi_g = ABNORMAL_PREDICTION_THRESHOLD
 
@@ -97,18 +179,18 @@ def gateway_adaptive_inference_heuristic(redis_client: redis.Redis, gateway_name
     print(f"Low battery detected: {low_battery}")
     print("=================================")
 
-    if q_t >= psi_q:    #  inference queue is full => cloud
+    if q_t >= psi_q:  #  inference queue is full => cloud
         return CLOUD_INFERENCE_LAYER
-    else: # q_t < psi_q
-        if u_t < m: # => M_t is not full
+    else:  # q_t < psi_q
+        if u_t < m:  # => M_t is not full
             return GATEWAY_INFERENCE_LAYER
-        else: # u_t >= m => M_t is full
+        else:  # u_t >= m => M_t is full
             if sigma_M_t < phi_g:
-                if low_battery: # b_t < phi_b
+                if low_battery:  # b_t < phi_b
                     return GATEWAY_INFERENCE_LAYER
-                else: # b_t >= phi_b
+                else:  # b_t >= phi_b
                     return SENSOR_INFERENCE_LAYER
             elif phi_g <= sigma_M_t and sigma_M_t < psi_g:
                 return GATEWAY_INFERENCE_LAYER
-            else: # sigma_M_t >= psi_g
+            else:  # sigma_M_t >= psi_g
                 return CLOUD_INFERENCE_LAYER

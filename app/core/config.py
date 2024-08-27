9640275f24d4aecddb02ee75b25efe58b3149a03
Raul de la Fuente <raul.delafuente@ing.uchile.cl>
2024-08-27 15:54:09 -0400

    updated for latency experiment

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,11 @@ import json
 from dotenv import load_dotenv
 
 # Retrieve enviroment variables from .env file
-load_dotenv()
+#load_dotenv()
 
-SECRET_KEY: str = os.environ.get("SECRET_KEY")
-INFERENCE_MICROSERVICE_HOST: str = os.environ.get("INFERENCE_MICROSERVICE_HOST")
-INFERENCE_MICROSERVICE_PORT: int = int(os.environ.get("INFERENCE_MICROSERVICE_PORT"))
+SECRET_KEY: str = os.environ.get("SECRET_KEY", "secret_key")
+INFERENCE_MICROSERVICE_HOST: str = os.environ.get("INFERENCE_MICROSERVICE_HOST", "127.0.0.1")
+INFERENCE_MICROSERVICE_PORT: int = int(os.environ.get("INFERENCE_MICROSERVICE_PORT", 8005))
 INFERENCE_MICROSERVICE_URL: str = f"http://{INFERENCE_MICROSERVICE_HOST}:{INFERENCE_MICROSERVICE_PORT}/api/v1"
 
 CLOUD_INFERENCE_LAYER = 2
@@ -15,14 +15,14 @@ GATEWAY_INFERENCE_LAYER = 1
 SENSOR_INFERENCE_LAYER = 0
 HEURISTIC_ERROR_CODE = -1
 
-ADAPTIVE_INFERENCE: bool = bool(int(os.environ.get("ADAPTIVE_INFERENCE", 1)))
-PREDICTION_HISTORY_LENGTH: int = int(os.environ.get("PREDICTION_HISTORY_LENGTH", 10))
+ADAPTIVE_INFERENCE: bool = bool(int(os.environ.get("ADAPTIVE_INFERENCE", 0)))
+PREDICTION_HISTORY_LENGTH: int = int(os.environ.get("PREDICTION_HISTORY_LENGTH", 16))
 NORMAL_PREDICTION_THRESHOLD: int = max(int(os.environ.get("NORMAL_PREDICTION_THRESHOLD", 2)), 1)
 ABNORMAL_PREDICTION_THRESHOLD: int = min(int(os.environ.get("ABNORMAL_PREDICTION_THRESHOLD", 8)), PREDICTION_HISTORY_LENGTH)
 ABNORMAL_LABELS = json.loads(os.environ.get("ABNORMAL_LABELS", "[2, 3]"))
 
-REDIS_HOST: str = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT: int = int(os.environ.get("REDIS_PORT", 6379))
+REDIS_HOST: str = os.environ.get("REDIS_HOST", "127.0.0.1")
+REDIS_PORT: int = int(os.environ.get("REDIS_PORT", 6381))
 REDIS_DB_CELERY_BROKER: int = int(os.environ.get("REDIS_DB_CELERY_BROKER", 0))
 REDIS_DB_CELERY_BACKEND: int = int(os.environ.get("REDIS_DB_CELERY_BACKEND", 1))
 REDIS_DB_HISTORY: int = int(os.environ.get("REDIS_DB_HISTORY", 2))
